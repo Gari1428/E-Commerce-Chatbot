5398d851572d743e0280dfb5b46a1bfe11c6896f
Gari1428 <garima991427@gmail.com>
2026-03-02 06:53:21 +0530

    Changes in Deployemnt code

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import streamlit as st
-from faq import ingest_faq_data, faq_chain
+from faq import (
+    ingest_faq_data, 
+    faq_chain, 
+    faqs_path,
+)
 from sql import sql_chain
 from pathlib import Path
-from router import router
+from router import (
+    SemanticRouter,
+    faq,
+    sql,
+    encoder
+)
 
-faqs_path = Path(__file__).parent / "resources"/"faqs.csv"
-ingest_faq_data(faqs_path)
 
+@st.cache_resource
+def initialize():
+    ingest_faq_data(faqs_path)
+    router = SemanticRouter(
+        routes=[faq, sql],
+        encoder=encoder,
+        auto_sync="local"
+    )
+    return router
+
+router = initialize()
 
 def ask(query):
+    print(f"Received query: {query}")
     route = router(query).name
+    print(f"Determined route: {route}")
     if route == 'faq':
         return faq_chain(query)
     elif route == 'sql':
